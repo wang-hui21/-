b0fce6c16a28bc2367d33ddf4784986b62efc3ab
wang-hui21 <1024099045@qq.com>
2023-08-19 10:37:01 +0800

    数据类型学习

---
+++ 第一章/src/Evaluate.java
@@ -0,0 +1,30 @@
+public class Evaluate {
+    public static void main(String[] args){
+        Stack<String> ops= new Stack<String>();
+        Stack<Double> vals=new Stack<Double>();
+        while(!StdIn.isEmpty()){
+            //读取字符，如果是运算符则压入栈中
+            String s =StdIn.readString();
+            if (s.equals("("));
+            else if(s.equals("+"))  ops.push(s);
+            else if(s.equals("-"))  ops.push(s);
+            else if(s.equals("*"))  ops.push(s);
+            else if(s.equals("/"))  ops.push(s);
+            else if(s.equals("sqrt")) ops.push(s);
+            else if (s.equals(")"))
+            {
+                //如果字符为")"，弹出运算符和操作数，计算结果并压入栈中
+                String op=ops.pop();
+                double v=vals.pop();
+                if (op.equals("+")) v=vals.pop()+v;
+                else if (op.equals("-")) v=vals.pop()-v;
+                else if (op.equals("*")) v=vals.pop()*v;
+                else if (op.equals("/")) v=vals.pop()/v;
+                else if(op.equals("sqrt")) v=Math.sqrt(v);
+                vals.push(v);
+            }
+            else vals.push(Double.parseDouble(s));
+        }
+        StdOut.println(vals.pop());
+    }
+}
